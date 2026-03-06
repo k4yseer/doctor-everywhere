bd4ceab52f0c280057f71f4abd32b20341a2a5a2
cassiaaaaa <cassia.tay.2024@computing.smu.edu.sg>
2026-03-06 18:00:36 +0800

    fix: email no longer flags the messages as scam

--- backend/atomic/notifications/app/main.py
+++ backend/atomic/notifications/app/main.py
@@ -17,17 +17,27 @@ def is_valid_email(email):
 @app.route("/notification/head-of-queue", methods=['POST'])
 def head_of_queue():
     patient_email = request.json.get('email')
+    meeting_link = request.json.get('meeting_link')
+
+    if meeting_link is None or patient_email is None:
+        return jsonify({
+            "error": "Missing required fields"
+        }), 400
+
     if not is_valid_email(patient_email):
         return jsonify({
             "error": "Invalid email"
         }), 400
 
-    meeting_link = request.json.get('meeting_link')
     params: resend.Emails.SendParams = {
         "from": f"Doctor Everywhere <notifications@{domain}>",
         "to": [f"{patient_email}"],
-        "subject": "You're at the front of the queue",
-        "html": f"Click here to join the meeting: {meeting_link}",
+        "subject": "Doctor Everywhere Consultation Link",
+        "html": f"""
+            <p>Dear Patient,<p>
+            <p>Click here to join the consultation: {meeting_link}. Thank you for your patience.</p>
+        """,
+        "text": "Please click the link to join the consultation. "
     }
 
     try:
@@ -46,15 +56,17 @@ def head_of_queue():
 @app.route("/notification/send-mc", methods=['POST'])
 def send_mc():
     patient_email = request.form.get('email')
+    appointment_id = request.form.get('appointment_id')
+    mc = request.files.get("file")
+    
+    if appointment_id is None or mc is None or patient_email is None:
+        return jsonify({
+            "error": "Missing required fields"
+        }), 400
+
     if not is_valid_email(patient_email):
         return jsonify({
             "error": "Invalid email"
-        }), 400
-
-    mc = request.files.get("file")
-    if not mc:
-        return jsonify({
-            "error": "No file provided"
         }), 400
 
     file_bytes: bytes = mc.read()
@@ -68,8 +80,17 @@ def send_mc():
     params: resend.Emails.SendParams = {
         "from": f"Doctor Everywhere <notifications@{domain}>",
         "to": [f"{patient_email}"],
-        "subject": "Medical Certificate",
-        "html": "Attached is your MC.",
+        "subject": f"Medical Certificate (Appointment #{appointment_id})",
+        "html": f"""
+            <p>Dear Patient,<p>
+            <p>Attached is your MC for for Appointment <strong>#{appointment_id}</strong>.<p>
+            <p>You may view your prescription and delivery details directly on the <strong>Doctor Everywhere</strong> app</p>
+            <p>Warm regards, <br/><strong>Doctor Everywhere</strong></P>
+        """,
+        "text": f"""
+            Attached is your MC for Appointment #{appointment_id}. 
+            View your prescription and delivery details on the Doctor Everywhere app.
+        """,
         "attachments": [attachment],
     }
 
@@ -89,20 +110,55 @@ def send_mc():
 @app.route("/notification/payment-details", methods=['POST'])
 def payment_details():
     patient_email = request.json.get('email')
+    appointment_id = request.json.get('appointment_id')
+    is_successful = request.json.get('is_successful')
+
+    if appointment_id is None or is_successful is None or patient_email is None:
+        return jsonify({
+            "error": "Missing required fields"
+        }), 400
+
     if not is_valid_email(patient_email):
         return jsonify({
             "error": "Invalid email"
         }), 400
 
-    appointment_id = request.json.get('appointment_id')
-    is_successful = request.json.get('is_successful')
+    subject = ""
+    html = ""
+    text = ""
+
+    if is_successful:
+        subject = f"Payment Received (Appointment #{appointment_id})"
+        html = f"""
+            <p>Dear Patient,<p>
+            <p>We're writing to confirm that your payment for Appointment <strong>#{appointment_id}</strong>
+            has been successfully processed.</p>
+            <p>You may view your prescription and delivery details directly on the <strong>Doctor Everywhere</strong> app</p>
+            <p>Warm regards, <br/><strong>Doctor Everywhere</strong></P>
+        """
+        text = f"""
+            Your payment for Appointment #{appointment_id} was successful. 
+            View your prescription and delivery details on the Doctor Everywhere app.
+        """
+    else:
+        subject = f"Payment Unsuccessful (Appointment #{appointment_id})"
+        html = f"""
+            <p>Dear Patient,<p>
+            <p>We were unable to process your payment for Appointment <strong>#{appointment_id}</strong></p>
+            <p>Please open the <strong>Doctor Everywhere</strong> app to review your payment details and try again at your convenience.</p>
+            <p>Warm regards, <br/><strong>Doctor Everywhere</strong></P>
+        """
+        text = f"""
+            Your payment for Appointment #{appointment_id} was unsuccessful. 
+            Please retry via Doctor Everywhere app.
+        """
 
     params: resend.Emails.SendParams = {
         "from": f"Doctor Everywhere <notifications@{domain}>",
         "to": [f"{patient_email}"],
-        "subject": f"Appointment {appointment_id} Payment Confirmation",
-        "html": f"Your payment has been {'successful. You may view your prescription/delivery details on the app.' if is_successful 
-                else 'unsuccessful. Please try again.'}",
+        "subject": subject,
+        "html": html,
+        "text": text
     }
 
     try:

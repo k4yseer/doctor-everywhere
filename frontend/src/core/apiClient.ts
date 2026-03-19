82fdab7dc9f6618e9c13a7d0ad6e9eeb3ca88915
minnong <ongminn@gmail.com>
2026-03-20 00:07:01 +0800

    feat: add API layer

--- frontend/src/core/apiClient.ts
+++ frontend/src/core/apiClient.ts
@@ -8,3 +8,35 @@ const apiClient = axios.create({
     },
     timeout: 10000, // drop request if BE hangs after 10 sec
 })
+
+// axios request interceptor
+apiClient.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('accessToken') // get auth token
+
+        if (token && config.headers) { // if token exists
+            config.headers.Authorization = `Bearer ${token}` // add token to auth header
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// axios response interceptor
+apiClient.interceptors.response.use(
+    (response) => { // if succeed
+        return response; // pass data
+    },
+    (error) => {
+        if (error.response && error.response.status === 401) { // if unauthorised
+            console.log("Unauthorised access. Token may have expired.")
+            // TODO: trigger silent token refresh or redirect to /login route here
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default apiClient;
